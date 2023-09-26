c551b175cc79891a6be5c02485c390f291897cca
Damián López <lopezrunco@gmail.com>
2023-09-26 15:32:05 +0530

    yaguari-set-2023

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var csvFile = File('~/Desktop/ost-list-caravana.csv')
+var csvFile = File('~/Desktop/LOTES REMATE SJY 2023.xlsx - toros hereford.csv')
 
 var csvData = []
 
@@ -14,9 +14,24 @@ csvFile.close()
 for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
     // Set content in the layers
     var thisCSVRow = csvData[csvDataIdx].split(',')
-    setContentInLayer(thisCSVRow[0], '_rp')
-    setContentInLayer('Nombre: ' + thisCSVRow[1] + ' | Nac. ' + thisCSVRow[2], '_first_line')
-    setContentInLayer('Padre: ' + thisCSVRow[3] + ' | CE: ' + thisCSVRow[4] + ' | Peso: ' + thisCSVRow[5], '_second_line')
+    setContentInLayer('RP: ' + thisCSVRow[0], '_rp')
+    setContentInLayer('Lote: ' + thisCSVRow[1], '_lote')
+
+    // LOTES REMATE SJY 2023.xlsx - HEMBRAS ANGUS .csv
+    // setContentInLayer('NAC: ' + thisCSVRow[2] + '    DEST: ' + thisCSVRow[3] + '     LECHE: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6], '_data1')
+    // setContentInLayer('CE: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data2')
+
+    // LOTES REMATE SJY 2023.xlsx - HEMBRAS ANGUS .csv
+    // setContentInLayer('FPD: ' + thisCSVRow[2] + '    NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6] + '     FPM: ' + thisCSVRow[7] + '     HL: ' + thisCSVRow[8], '_data1')
+    // setContentInLayer('AOB: ' + thisCSVRow[9] + '    GR: ' + thisCSVRow[10] + '     MAR: ' + thisCSVRow[11] + '     CE: ' + thisCSVRow[12] + '     ICr: ' + thisCSVRow[13] + '     ICo: ' + thisCSVRow[14], '_data2')
+
+    // LOTES REMATE SJY 2023.xlsx - toros angus .csv
+    // setContentInLayer('NAC: ' + thisCSVRow[2] + '    DEST: ' + thisCSVRow[3] + '     HL: ' + thisCSVRow[4] + '     18M: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6], '_data1')
+    // setContentInLayer('CE: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data2')
+
+    // LOTES REMATE SJY 2023.xlsx - toros hereford.csv
+    setContentInLayer('FPD: ' + thisCSVRow[2] + '    NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6] + '     FPM: ' + thisCSVRow[7] + '     HL: ' + thisCSVRow[8], '_data1')
+    setContentInLayer('AOB: ' + thisCSVRow[9] + '    GR: ' + thisCSVRow[10] + '     MAR: ' + thisCSVRow[11] + '     CE: ' + thisCSVRow[12] + '     ICr: ' + thisCSVRow[13] + '     ICo: ' + thisCSVRow[14], '_data2')
 
     // Apply template and customize path & file name
     var item = app.project.renderQueue.items.add(app.project.activeItem)
@@ -27,7 +42,7 @@ for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
 
     app.project.renderQueue.render()
 }
-alert('All renders are finished')
+alert('Done!')
 
 function setContentInLayer(content, layer) {
     // Select active compostition

20ab7308a4525346efdca167bc9c363376a9adcb
Damián López <lopezrunco@gmail.com>
2023-09-29 16:35:03 +0530

    baqueano-set-2023

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var csvFile = File('~/Desktop/LOTES REMATE SJY 2023.xlsx - toros hereford.csv')
+var csvFile = File('~/Desktop/Baqueano.xlsx - Hoja1.csv')
 
 var csvData = []
 
@@ -14,30 +14,16 @@ csvFile.close()
 for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
     // Set content in the layers
     var thisCSVRow = csvData[csvDataIdx].split(',')
-    setContentInLayer('RP: ' + thisCSVRow[0], '_rp')
-    setContentInLayer('Lote: ' + thisCSVRow[1], '_lote')
-
-    // LOTES REMATE SJY 2023.xlsx - HEMBRAS ANGUS .csv
-    // setContentInLayer('NAC: ' + thisCSVRow[2] + '    DEST: ' + thisCSVRow[3] + '     LECHE: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6], '_data1')
-    // setContentInLayer('CE: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data2')
-
-    // LOTES REMATE SJY 2023.xlsx - HEMBRAS ANGUS .csv
-    // setContentInLayer('FPD: ' + thisCSVRow[2] + '    NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6] + '     FPM: ' + thisCSVRow[7] + '     HL: ' + thisCSVRow[8], '_data1')
-    // setContentInLayer('AOB: ' + thisCSVRow[9] + '    GR: ' + thisCSVRow[10] + '     MAR: ' + thisCSVRow[11] + '     CE: ' + thisCSVRow[12] + '     ICr: ' + thisCSVRow[13] + '     ICo: ' + thisCSVRow[14], '_data2')
-
-    // LOTES REMATE SJY 2023.xlsx - toros angus .csv
-    // setContentInLayer('NAC: ' + thisCSVRow[2] + '    DEST: ' + thisCSVRow[3] + '     HL: ' + thisCSVRow[4] + '     18M: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6], '_data1')
-    // setContentInLayer('CE: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data2')
-
-    // LOTES REMATE SJY 2023.xlsx - toros hereford.csv
-    setContentInLayer('FPD: ' + thisCSVRow[2] + '    NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PA_V: ' + thisCSVRow[6] + '     FPM: ' + thisCSVRow[7] + '     HL: ' + thisCSVRow[8], '_data1')
-    setContentInLayer('AOB: ' + thisCSVRow[9] + '    GR: ' + thisCSVRow[10] + '     MAR: ' + thisCSVRow[11] + '     CE: ' + thisCSVRow[12] + '     ICr: ' + thisCSVRow[13] + '     ICo: ' + thisCSVRow[14], '_data2')
+    setContentInLayer(thisCSVRow[0], '_rp')
+    setContentInLayer('Padre: ' + thisCSVRow[1], '_padre')
+    setContentInLayer('PIG (I/D): ' + thisCSVRow[2] + '     NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18M: ' + thisCSVRow[5] + '     LECHE: ' + thisCSVRow[6], '_data1')
+    setContentInLayer('AOBc: ' + thisCSVRow[7] + '     C.ESC: ' + thisCSVRow[8] + '     ICR: ' + thisCSVRow[9] + '     Icomp: ' + thisCSVRow[10], '_data2')
 
     // Apply template and customize path & file name
     var item = app.project.renderQueue.items.add(app.project.activeItem)
     var outputModule = item.outputModule(1)
     var outputFolder = "~/Desktop/rendered/"
-    outputModule.applyTemplate("PNGAlfa")
+    outputModule.applyTemplate("High Quality with Alpha")
     outputModule.file = File(outputFolder + thisCSVRow[0])
 
     app.project.renderQueue.render()

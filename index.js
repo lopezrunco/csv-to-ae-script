68cba303bf405a74e9bdf61c4a62e960645a3a8f
Damián López <lopezrunco@gmail.com>
2023-10-02 19:39:26 +0530

    lots with only rp & pig

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var csvFile = File('~/Desktop/Baqueano.xlsx - Hoja1.csv')
+var csvFile = File('~/Desktop/Baqueano-lotes-especiales.csv')
 
 var csvData = []
 
@@ -15,14 +15,12 @@ for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
     // Set content in the layers
     var thisCSVRow = csvData[csvDataIdx].split(',')
     setContentInLayer(thisCSVRow[0], '_rp')
-    setContentInLayer('Padre: ' + thisCSVRow[1], '_padre')
-    setContentInLayer('PIG (I/D): ' + thisCSVRow[2] + '     NAC: ' + thisCSVRow[3] + '     DEST: ' + thisCSVRow[4] + '     18M: ' + thisCSVRow[5] + '     LECHE: ' + thisCSVRow[6], '_data1')
-    setContentInLayer('AOBc: ' + thisCSVRow[7] + '     C.ESC: ' + thisCSVRow[8] + '     ICR: ' + thisCSVRow[9] + '     Icomp: ' + thisCSVRow[10], '_data2')
+    setContentInLayer(thisCSVRow[1], '_pig')
 
     // Apply template and customize path & file name
     var item = app.project.renderQueue.items.add(app.project.activeItem)
     var outputModule = item.outputModule(1)
-    var outputFolder = "~/Desktop/rendered/"
+    var outputFolder = "~/Desktop/mograph/"
     outputModule.applyTemplate("High Quality with Alpha")
     outputModule.file = File(outputFolder + thisCSVRow[0])
 

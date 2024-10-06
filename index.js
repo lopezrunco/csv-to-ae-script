a72a40e41595bfb4eba0a46b1c5d00f42b411b42
viejo bucanero <lopezrunco@gmail.com>
2024-10-06 09:25:21 +0530

    feat: Add el baqueano oct 2024.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var csvFile = File('~/Desktop/Baqueano-lotes-especiales.csv')
+var csvFile = File('~/Desktop/list.csv')
 
 var csvData = []
 
@@ -15,7 +15,14 @@ for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
     // Set content in the layers
     var thisCSVRow = csvData[csvDataIdx].split(',')
     setContentInLayer(thisCSVRow[0], '_rp')
-    setContentInLayer(thisCSVRow[1], '_pig')
+    setContentInLayer('Padre: ' + thisCSVRow[1], '_padre')
+    setContentInLayer('Peso al nacer: ' + thisCSVRow[2] + ' | ' + 'Peso al destete: ' + thisCSVRow[3] + ' | ' + 'Peso de 18 meses: ' + thisCSVRow[4], '_data1')
+
+    // list 1
+    setContentInLayer('LECHE: ' + thisCSVRow[5] + ' | ' + 'AOBc: ' + thisCSVRow[6] + ' | ' + 'CE(cm): ' + thisCSVRow[7] + ' | ' + 'Pig: ' + thisCSVRow[8], '_data2')
+
+    // list 2
+    // setContentInLayer('LECHE: ' + thisCSVRow[5] + ' | ' + 'AOBc: ' + thisCSVRow[6] + ' | ' + 'MARB: ' + thisCSVRow[7] + ' | ' + 'C.ESC: ' + thisCSVRow[8], '_data2')
 
     // Apply template and customize path & file name
     var item = app.project.renderQueue.items.add(app.project.activeItem)

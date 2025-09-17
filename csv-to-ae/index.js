eafc15ffddbf00238f35b29780137533b9619e2d
lopezrunco <lopezrunco@gmail.com>
2025-09-17 11:17:40 -0300

    refactor: Update yaguari csv-to-png 2025.

--- csv-to-ae/index.js
+++ csv-to-ae/index.js
@@ -1,4 +1,4 @@
-var csvFile = File('~/Desktop/toros-angus.csv')
+var csvFile = File('~/Desktop/hembras-angus.csv')
 
 var csvData = []
 
@@ -18,19 +18,19 @@ for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
     setContentInLayer('RP: ' + thisCSVRow[0], '_rp')
 
     // Angus
-    setContentInLayer('PN: ' + thisCSVRow[2] + '    PD: ' + thisCSVRow[3] + '     P18m: ' + thisCSVRow[4] + '     PAV: ' + thisCSVRow[5] + '     CE: ' + thisCSVRow[6], '_data1')
-    setContentInLayer('HL: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data1')
+    // setContentInLayer('PN: ' + thisCSVRow[2] + '    PD: ' + thisCSVRow[3] + '     P18m: ' + thisCSVRow[4] + '     PAV: ' + thisCSVRow[5] + '     CE: ' + thisCSVRow[6], '_data1')
+    // setContentInLayer('HL: ' + thisCSVRow[7] + '    AOB: ' + thisCSVRow[8] + '     EGS: ' + thisCSVRow[9] + '     MARB: ' + thisCSVRow[10], '_data2')
 
     // Hereford
-    // setContentInLayer('FPd: ' + thisCSVRow[2] + '    PN: ' + thisCSVRow[3] + '     PD: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5], '_data1')
-    // setContentInLayer('PAV: ' + thisCSVRow[6] + '    AOB: ' + thisCSVRow[7] + '     GR: ' + thisCSVRow[8] + '     CE: ' + thisCSVRow[9], '_data2')
+    setContentInLayer('FPd: ' + thisCSVRow[2] + '    PN: ' + thisCSVRow[3] + '     PD: ' + thisCSVRow[4] + '     18m: ' + thisCSVRow[5] + '     PAV: ' + thisCSVRow[6] + '     FPm: ' + thisCSVRow[7] + '     HL: ' + thisCSVRow[8], '_data1')
+    setContentInLayer('AOB: ' + thisCSVRow[9] + '    GR: ' + thisCSVRow[10] + '     MAR: ' + thisCSVRow[11] + '     CE: ' + thisCSVRow[12] + '     Icr: ' + thisCSVRow[13] + '     Ico: ' + thisCSVRow[14], '_data2')
 
     // Apply template and customize path & file name
     var item = app.project.renderQueue.items.add(app.project.activeItem)
     var outputModule = item.outputModule(1)
     var outputFolder = "~/Desktop/rendered/"
     outputModule.applyTemplate("pngalfa")
-    outputModule.file = File(outputFolder + 'Lote: ' + thisCSVRow[1] + ' RP: ' + thisCSVRow[0])
+    outputModule.file = File(outputFolder + 'Lote ' + thisCSVRow[1] + ' RP ' + thisCSVRow[0])
 
     app.project.renderQueue.render()
 }

8942e2922629b1749b697292f435e7ff407b038f
lopezrunco <lopezrunco@gmail.com>
2025-10-04 01:06:26 -0300

    refactor: Add elbaqueano 2025.

--- csv-to-ae/index.js
+++ csv-to-ae/index.js
@@ -1,4 +1,5 @@
-var csvFile = File('~/Documents/list.csv')
+var csvFile = File('~/Desktop/list.csv')
+
 var csvData = []
 
 // Read line by line and push the data in csvData
@@ -10,28 +11,33 @@ do {
 } while (!csvFile.eof)
 csvFile.close()
 
-// Loops the csvData array, and for every row set the content in the selected text layer and render the comp 
 for (var csvDataIdx = 1; csvDataIdx < csvData.length; csvDataIdx++) {
+    // Set content in the layers
     var thisCSVRow = csvData[csvDataIdx].split(',')
     setContentInLayer(thisCSVRow[0], '_rp')
-    setContentInLayer(thisCSVRow[1], '_fechanac')
-    setContentInLayer(thisCSVRow[2], '_padre')
-    setContentInLayer(thisCSVRow[3], '_madre')
-    setContentInLayer(thisCSVRow[4], '_lact')
-    setContentInLayer(thisCSVRow[5], '_edad')
-    setContentInLayer(thisCSVRow[6], '_dias')
-    setContentInLayer(thisCSVRow[7], '_leche')
-    setContentInLayer(thisCSVRow[8], '_grasa')
-    setContentInLayer(thisCSVRow[9], '_proteina')
-    app.project.item(1).name = thisCSVRow[0] // Set the value of 'rp' field as the output name
-    app.project.renderQueue.items.add(app.project.item(1))
+    setContentInLayer('RP Padre: ' + thisCSVRow[1], '_padre')
+    setContentInLayer('Peso al nacer: ' + thisCSVRow[2] + ' | ' + 'Peso al destete: ' + thisCSVRow[3] + ' | ' + 'Peso de 18 meses: ' + thisCSVRow[4], '_data1')
+
+    // list 1
+    setContentInLayer('LECHE: ' + thisCSVRow[5] + ' | ' + 'AOB: ' + thisCSVRow[6] + ' | ' + 'CE(cm): ' + thisCSVRow[7] + ' | ' + 'Pig: ' + thisCSVRow[8], '_data2')
+
+    // list 2
+    // setContentInLayer('LECHE: ' + thisCSVRow[5] + ' | ' + 'AOB: ' + thisCSVRow[6] + ' | ' + 'MARB: ' + thisCSVRow[7] + ' | ' + 'C.ESC: ' + thisCSVRow[8], '_data2')
+
+    // Apply template and customize path & file name
+    var item = app.project.renderQueue.items.add(app.project.activeItem)
+    var outputModule = item.outputModule(1)
+    var outputFolder = "~/Desktop/rendered/"
+    outputModule.applyTemplate("High Quality with Alpha")
+    outputModule.file = File(outputFolder + thisCSVRow[0])
+
     app.project.renderQueue.render()
 }
-alert('All renders are finished')
+alert('Done!')
 
 function setContentInLayer(content, layer) {
-    // Select _render comp
-    var comp = app.project.item(1)
+    // Select active compostition
+    var comp = app.project.activeItem
     // Select layer and set new text
     var layer = comp.layer(layer)
     var textProp = layer.property("Source Text")
